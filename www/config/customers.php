119cde8b32e7f12bc3f6a74acff8e01d38f4adc8
Ibrahim Israfilov <90762709+ibritics@users.noreply.github.com>
2023-12-19 18:47:00 +0100

    Delete www directory

--- www/config/customers.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include('config/db_connect.php');  //Connection to the db taken from another module
-$sql = 'SELECT customer_id, email, location FROM customers';
-$result = mysqli_query($conn, $sql);
-$customers = mysqli_fetch_all($result, MYSQLI_ASSOC);
-mysqli_close($conn);
-?>
--- www/footer.php
+++
@@ -1,4 +0,0 @@
-<div class= "copyright-section">
-    Copyright 2023 Ibrahim Israfilov. 
-</div>
-</body>
